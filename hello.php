47978ebae27753fa0f757564fd815823697c6213
faiza119 <faizaquadri225@gmail.com>
2025-06-23 16:09:20 +0530

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,21 @@
+<?php
+// Single-line comment
+
+/*
+ Multi-line comment
+*/
+
+echo "Hello, World!<br>";
+print "Welcome to PHP Programming!<br>";
+
+// Variable declaration
+$name = "Faiza";
+$age = 20;
+$height = 5.3;
+$isStudent = true;
+
+echo "Name: $name<br>";
+echo "Age: $age<br>";
+echo "Height: $height<br>";
+echo "Student: " . ($isStudent ? "Yes" : "No") . "<br>";
+?>
